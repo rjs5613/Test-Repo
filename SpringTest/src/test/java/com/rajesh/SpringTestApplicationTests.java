49946c1fc69e35824c59199f80a40ac7ad22fc59
Rajesh Kumar <rjs5613@gmail.com>
2016-01-26 11:50:31 +0530

    Test Case to check the db update

--- SpringTest/src/test/java/com/rajesh/SpringTestApplicationTests.java
+++ SpringTest/src/test/java/com/rajesh/SpringTestApplicationTests.java
@@ -2,17 +2,27 @@ package com.rajesh;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.rajesh.student.StudentService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpringTestApplication.class)
 @WebAppConfiguration
 public class SpringTestApplicationTests {
 
+	@Autowired
+	private StudentService studentService;
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testStudent() {
+		System.out.println(studentService.findById(1).getName());
 	}
 
 }
